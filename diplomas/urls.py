1da1c3b1f2f110aaf657975c42ea675beeeb1597
Oleg Kainov <kainov.oleg@gmail.com>
2016-03-22 21:30:11 +0300

    Maybe it will help with static

--- diplomas/urls.py
+++ diplomas/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'diplomas'
 urlpatterns = [
     url(r'^geoloto$', views.geoloto, name='geoloto'),
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
 ]
