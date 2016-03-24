1c557512c106219d14905f3b67e226bf6e94057f
Oleg Kainov <kainov.oleg@gmail.com>
2016-03-24 09:10:03 +0300

    Fixed api usage Added correct typing for blue and red caches

--- diplomas/geocaching/api.py
+++ diplomas/geocaching/api.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
+import re
 
 import urllib.request
-import xml
+import xml.etree.ElementTree
 from diplomas.geocaching.cache import Cache
-from diplomas.geocaching.utils import extract_nickname_from_webpage, extract_caches_from_webpage
 
 
 def get_page_by_url(url):
@@ -11,11 +11,13 @@ def get_page_by_url(url):
     bytes_str = r.read()
     return bytes_str.decode('cp1251')
 
+def get_page_by_url_without_encoding(url):
+    return urllib.request.urlopen(url).read()
 
 def get_cache_details(cache_id):
-    page_src = get_page_by_url('http://www.geocaching.su/site/api.php?rtype=2&cid=%s&istr=ems' % cache_id).read()
+    page_src = get_page_by_url_without_encoding('http://www.geocaching.su/site/api.php?rtype=2&cid=%s&istr=ems' % cache_id)
     root = xml.etree.ElementTree.fromstring(page_src)
-    return Cache(root)
+    return Cache.init_from_xml(root)
 
 
 def get_user_finds(user_id):
@@ -31,3 +33,46 @@ def get_user_creations(user_id):
 def get_user_nickname(user_id):
     page_src = get_page_by_url("http://www.geocaching.su/site/popup/userstat.php?s=2&uid=%s" % user_id)
     return extract_nickname_from_webpage(page_src)
+
+
+def extract_nickname_from_webpage(page_src):
+    cache_re = re.compile('<h1 class=hdr>(.+?)</h1>', flags=re.DOTALL | re.UNICODE)
+    try:
+        return cache_re.findall(page_src)[0]
+    except:
+        return None
+
+
+def extract_caches_from_webpage(page_src):
+    re_for_created_cache = re.compile('(?P<type>\w\w).{0,1}\.png.*?<a href=.*?pn=101&cid=(?P<id>\d+).*?blank>(?P<name>.*?)</a>.*?(?P<cdate>\d\d.\d\d.\d\d\d\d), (?P<region>.*?)[\d<]', flags=re.DOTALL | re.UNICODE)
+    re_for_found_cache = re.compile('<tr><td>.*?(?P<type>\w\w).{0,1}\.png.*?<a href=.*?pn=101&cid=(?P<id>\d+).*?blank>(?P<name>.*?)</a>.*?<b>(?P<creator>.*?)</b>.*?\((?P<cdate>\d\d.\d\d.\d\d\d\d), (?P<region>.*?)\)', flags=re.DOTALL | re.UNICODE)
+    if 'Созданные тайники' in page_src:
+        cache_re = re_for_created_cache
+    else:
+        cache_re = re_for_found_cache
+
+    caches = list()
+    n_all_finds = len(cache_re.findall(page_src))
+    for i, re_match in enumerate(cache_re.finditer(page_src)):
+
+        cache_id = re_match.group('id')
+        cache_type = re_match.group('type')
+        try:
+            cache_creator = re_match.group('creator')
+        except IndexError:
+            cache_creator = extract_nickname_from_webpage(page_src)
+        cache_name = re_match.group('name')
+        creation_date = re_match.group('cdate')
+        region = re_match.group('region')
+        if 'рейтинг' in region:
+            region = region[:-10]
+
+        if cache_type.lower() in ['lu', 're']:
+            temp_cache = get_cache_details(cache_id)
+            cache_type = temp_cache.cache_type
+
+        if i % 100 == 0:
+            print('%s\\%s' % (i, n_all_finds))
+        current_cache = Cache.init_from_fields(cache_id, cache_type, cache_name, cache_creator, creation_date, region)
+        caches.append(current_cache)
+    return caches
--- diplomas/geocaching/cache.py
+++ diplomas/geocaching/cache.py
@@ -8,18 +8,32 @@ cache_types_lookup = {'Традиционный': 'TR',
 
 class Cache:
 
-    def __init__(self, xml_data):
-        self.cache_type = cache_types_lookup[xml_data.findall('type')[0].text]
-        self.cache_id = xml_data.findall('id')[0].text
-        self.name = xml_data.findall('name')[0].text
+    cache_type = None
+    cache_id = None
+    name = None
+    author = None
+    region = None
+    creation_date = None
 
-    def __init__(self, id, type, name, author, creation_date, region):
-        self.cache_type = type.upper()
-        self.cache_id = id
-        self.name = name
-        self.author = author
-        self.region = region
-        self.creation_date = creation_date
+    @classmethod
+    def init_from_xml(cls, xml_data):
+        cache = Cache()
+        cache.cache_type = cache_types_lookup[xml_data.findall('type')[0].text]
+        cache.cache_id = xml_data.findall('id')[0].text
+        cache.name = xml_data.findall('name')[0].text
+        cache.creation_date = xml_data.findall('date')[0].text
+        return cache
+
+    @classmethod
+    def init_from_fields(cls, id, type, name, author, creation_date, region):
+        cache = Cache()
+        cache.cache_type = type.upper()
+        cache.cache_id = id
+        cache.name = name
+        cache.author = author
+        cache.region = region
+        cache.creation_date = creation_date
+        return cache
 
     def __repr__(self):
         return '%s/%s: %s' % (self.cache_type, self.cache_id, self.name)
